40d644eff2e97e664930972be378718e28535138
mallan67 <mayad67@gmail.com>
2025-12-16 11:01:59 +0000

    Finalize Prisma removal

--- src/app/api/_debug/env/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    ADMIN_EMAIL: process.env.ADMIN_EMAIL ?? null,
-    HAS_SESSION_SECRET: Boolean(process.env.SESSION_SECRET),
-  });
-}
