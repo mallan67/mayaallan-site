6ee21fee01cb13d22c254e533981fea4fcd15351
mallan67 <mayad67@gmail.com>
2025-12-20 21:54:04 +0000

    chore(migrations): make 0001_shiny_epoch ALTERs safe (IF EXISTS, schema-qualified)

--- drizzle/0001_shiny_epoch.sql
+++ drizzle/0001_shiny_epoch.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "book" ALTER COLUMN "tags" SET DEFAULT '[]'::jsonb;--> statement-breakpoint
-ALTER TABLE "book" ALTER COLUMN "sales_metadata" SET DEFAULT '{}'::jsonb;--> statement-breakpoint
-ALTER TABLE "book" ALTER COLUMN "seo" SET DEFAULT '{}'::jsonb;--> statement-breakpoint
-ALTER TABLE "book_retailer_link" ALTER COLUMN "types" SET DEFAULT '[]'::jsonb;
+ALTER TABLE IF EXISTS public.book ALTER COLUMN "tags" SET DEFAULT '[]'::jsonb;--> statement-breakpoint
+ALTER TABLE IF EXISTS public.book ALTER COLUMN "sales_metadata" SET DEFAULT '{}'::jsonb;--> statement-breakpoint
+ALTER TABLE IF EXISTS public.book ALTER COLUMN "seo" SET DEFAULT '{}'::jsonb;--> statement-breakpoint
+ALTER TABLE IF EXISTS public.book_retailer_link ALTER COLUMN "types" SET DEFAULT '[]'::jsonb;
